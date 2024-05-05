32a6d030a8c34b0548eeb840957fba14e1746db5
ccrsxx <aminrisal@gmail.com>
2024-05-06 00:21:07 +0700

    feat: add pc ip address for shutdown command

--- src/scripts/shutdown.py
+++ src/scripts/shutdown.py
@@ -6,14 +6,15 @@ from typing import Final
 
 PC_USERNAME: Final = os.getenv("PC_USERNAME")
 PC_PASSWORD: Final = os.getenv("PC_PASSWORD")
+PC_IP_ADDRESS: Final = os.getenv("PC_IP_ADDRESS")
 
 
 def main() -> None:
-    subprocess.run(["shutdown", "/s", "/t", "60", "/c", "From UPS"])
+    subprocess.run(["shutdown", "/s", "/f", "/t", "60", "/c", "From UPS"])
 
     # Turn off PC if regardless if it's on or off
-    subprocess.run(["net", "use", "\\\PC\\IPC$", PC_PASSWORD, f"/USER:{PC_USERNAME}"])
-    subprocess.run(["shutdown", "/s", "/m", "pc", "/t", "60", "/c", "From UPS"])
+    subprocess.run(["net", "use", F"\\\{PC_IP_ADDRESS}\\IPC$", PC_PASSWORD, f"/USER:{PC_USERNAME}"])
+    subprocess.run(["shutdown", "/s", "/f", "/m", PC_IP_ADDRESS, "/t", "60", "/c", "From UPS"])
 
 
 if __name__ == "__main__":
