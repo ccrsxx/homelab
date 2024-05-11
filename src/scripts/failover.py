1fb86fee6a773d8dcf852c9c8ff44be932ac1119
ccrsxx <aminrisal@gmail.com>
2024-05-12 00:10:07 +0700

    feat: add ability to add incident on uptime kuma in event of failover

--- src/scripts/failover.py
+++ src/scripts/failover.py
@@ -1,8 +1,20 @@
+import os
 import sys
 import subprocess
 
 from typing import Final, Literal
 from utils.mail import send_email
+
+from uptime_kuma_api import UptimeKumaApi
+
+
+UPTIME_KUMA_API: Final = UptimeKumaApi(os.getenv("UPTIME_KUMA_URL"))
+
+UPTIME_KUMA_API.login(
+    os.getenv("UPTIME_KUMA_USERNAME"), os.getenv("UPTIME_KUMA_PASSWORD")
+)
+
+UPTIME_KUMA_MAINTENANCE_ID: Final = os.getenv("UPTIME_KUMA_MAINTENANCE_ID")
 
 
 def get_event() -> Literal["up", "down"]:
@@ -19,18 +31,32 @@ def get_event() -> Literal["up", "down"]:
     return event
 
 
+def trigger_up_event() -> None:
+    subprocess.run(["qbt", "torrent", "resume", "all"])
+
+    UPTIME_KUMA_API.pause_maintenance(UPTIME_KUMA_MAINTENANCE_ID)
+
+    send_email("Iconnet is up", "Primary WAN connection is up. Failover is over.")
+
+
+def trigger_down_event() -> None:
+    subprocess.run(["qbt", "torrent", "pause", "all"])
+
+    UPTIME_KUMA_API.resume_maintenance(UPTIME_KUMA_MAINTENANCE_ID)
+
+    send_email(
+        "Iconnet is down",
+        "Primary WAN connection is down. Using XL as a Failover now.",
+    )
+
+
 def main() -> None:
     event: Final = get_event()
 
     if event == "up":
-        subprocess.run(["qbt", "torrent", "resume", "all"])
-        send_email("Iconnet is up", "Primary WAN connection is up. Failover is over.")
+        trigger_up_event()
     else:
-        subprocess.run(["qbt", "torrent", "pause", "all"])
-        send_email(
-            "Iconnet is down",
-            "Primary WAN connection is down. Using XL as a Failover now.",
-        )
+        trigger_down_event()
 
 
 if __name__ == "__main__":
