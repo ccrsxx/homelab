86627a272cec72554102e04e67b387607f7b66c6
ccrsxx <aminrisal@gmail.com>
2024-10-21 00:57:42 +0700

    style: remove multiple new lines on the end of the file

--- script/bin/docker.sh
+++ script/bin/docker.sh
@@ -7,4 +7,3 @@ source venv/bin/activate
 python script/docker.py
 
 deactivate
-
--- script/bin/normalize_path.sh
+++ script/bin/normalize_path.sh
@@ -4,4 +4,3 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 cd ../..
-
