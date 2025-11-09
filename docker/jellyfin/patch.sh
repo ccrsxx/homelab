64b186547055fc7ade5621eab791eee52724f2ad
Ami <ami@ccrsxx.com>
2025-11-09 15:34:50 +0000

    fix: jellyfin patch not working on the jellyfin 10.11.2

--- docker/jellyfin/patch.sh
+++ docker/jellyfin/patch.sh
@@ -4,7 +4,7 @@ TARGET="/jellyfin/jellyfin-web/main.jellyfin.bundle.js"
 
 if [ -f "$TARGET" ]; then
   echo "Patching $TARGET ..."
-  sed -i 's/enableBackdrops:function(){return R}/enableBackdrops:function(){return E}/g' "$TARGET"
+  sed -i 's/enableBackdrops:function(){return D}/enableBackdrops:function(){return E}/g' "$TARGET"
 else
   echo "Warning: $TARGET not found, skipping patch"
 fi
