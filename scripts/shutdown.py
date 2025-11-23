abf50307cac2b07c6635653b8822f1373fcb9ef5
Ami <ami@ccrsxx.com>
2025-11-23 22:16:55 +0700

    feat: working shutdown script

--- scripts/shutdown.py
+++ scripts/shutdown.py
@@ -4,19 +4,10 @@ from utils.env import app_env
 
 
 def main() -> None:
-    subprocess.run(['shutdown', '/s', '/f', '/t', '60', '/c', 'From UPS'])
+    # Turn off Homelab
+    subprocess.run(['ssh', 'proxmox'])
 
     # Turn off PC if regardless if it's on or off
-    subprocess.run(
-        [
-            'net',
-            'use',
-            f'\\\{app_env.PC_IP_ADDRESS}\\IPC$',
-            app_env.PC_PASSWORD,
-            f'/USER:{app_env.PC_USERNAME}',
-        ]
-    )
-
     subprocess.run(
         [
             'shutdown',
