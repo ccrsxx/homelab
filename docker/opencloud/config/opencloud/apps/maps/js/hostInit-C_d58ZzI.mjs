9a8a249326cf2bd25afc2472e030b8c03294095d
Ami <ami@ccrsxx.com>
2026-09-13 12:35:15 +0000

    feat: add opencloud service

---
+++ docker/opencloud/config/opencloud/apps/maps/js/hostInit-C_d58ZzI.mjs
@@ -0,0 +1,1 @@
+import{t as e}from"./preload-helper-DYl5dUZ5.mjs";await(await e(()=>import(`./remoteEntry-BXGAhFe2.mjs`),[],import.meta.url)).init();
---
+++ docker/opencloud/config/opencloud/apps/maps/js/maps-z34tTD6Z.mjs
@@ -0,0 +1,1 @@
+import{t as e}from"./src-Bjkk5jHv.mjs";export{e as default};
